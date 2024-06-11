36907aad49daa1f04b6f8c17b55a1ca0d1bc58e6
HarshilGajera04 <132142466+HarshilGajera04@users.noreply.github.com>
2024-06-11 11:46:34 +0530

    1

---
+++ header copy.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <title> NEEDSOFREV - Your Gateway to Procurement</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="ThemeZaa">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <meta name="description" content="">
+    <!-- favicon icon -->
+    <link rel="shortcut icon" href="img/logo/favicon.jpg">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+    <!-- google fonts preconnect -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- style sheets and font icons  -->
+    <link rel="stylesheet" href="css/vendors.min.css" />
+    <link rel="stylesheet" href="css/icon.min.css" />
+    <link rel="stylesheet" href="css/style.css?bna=12" />
+    <link rel="stylesheet" href="css/responsive.css" />
+    <link rel="stylesheet" href="css/branding-agency.css" />
+</head>
+
+<body data-mobile-nav-trigger-alignment="right" data-mobile-nav-style="modern" data-mobile-nav-bg-color="#1d1d1d">
+    <!-- start header -->
+    <header>
+        <!-- start navigation -->
+        <nav class="navbar navbar-expand-lg header-light bg-white header-reverse glass-effect">
+            <div class="container-fluid">
+                <div class="col-auto col-lg-2 me-lg-0 me-auto">
+                    <a class="navbar-brand" href="index.php">
+                        <img src="img/logo/logo.png" data-at2x="img/logo/logo.png" alt="" class="default-logo">
+                        <img src="img/logo/logo.png" data-at2x="img/logo/logo.png" alt="" class="alt-logo">
+                        <img src="img/logo/logo.png" data-at2x="img/logo/logo.png" alt="" class="mobile-logo">
+                    </a>
+                </div>
+                <div class="col-auto ms-auto md-ms-0 menu-order position-static">
+                    <button class="navbar-toggler float-start" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-line"></span>
+                        <span class="navbar-toggler-line"></span>
+                        <span class="navbar-toggler-line"></span>
+                        <span class="navbar-toggler-line"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                        <ul class="navbar-nav alt-font">
+                            <li class="nav-item"><a href="about.php" class="nav-link">About Us</a></li>
+                            <li class="nav-item dropdown dropdown-with-icon-style02">
+                                <a href="services.php" class="nav-link">Services</a>
+                                <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink"
+                                    role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                                <!-- <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a href="#">Service 1</a></li>
+                                    <li><a href="#">Service 2</a></li>
+                                    <li><a href="#">Service 3</a></li>
+                                    <li><a href="#">Service 4</a></li>
+                                </ul> -->
+                            </li>
+                            <li class="nav-item"><a href="wecare.php" class="nav-link">We Care</a></li>
+                            <li class="nav-item"><a href="suppliers.php" class="nav-link">For Suppliers</a></li>
+                            <li class="nav-item"><a href="contact.php" class="nav-link">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-auto text-end d-none d-sm-flex">
+                    <div class="header-icon">
+                        <div class="header-button">
+                            <a href="mailto:info@domain.com"
+                                class="btn border-1 btn-transparent-light-gray btn-medium left-icon btn-switch-text">
+                                <span>
+                                    <span><i class="fa-regular fa-envelope"></i></span>
+                                    <span class="btn-double-text" data-text="Send a message">Send a message</span>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- end navigation -->
+    </header>
+    <!-- end header -->
